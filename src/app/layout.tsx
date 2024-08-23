897222151b5af4da773b0be8000c59f391ee7f59
Mohit Soni <mohitsoni9731@gmail.com>
2024-08-23 17:35:35 +0530

    Fized the bars and cross SVG colors  + Added transition when it changes the theme + Added NavItem component in place of Link in Mobile view in NavBar.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
    return (
       <html lang="en" style={{ fontSize: "62.5%" }}>
          <link rel="shortcut icon" href="/Avatar.png" type="image/x-icon" />
-         <body className={clsx(inter.className, 'bg-dot-white/[0.2]','bg-dot-black/[0.2] bg-[var(--backgroundColor)]  text-full')}>
+         <body className={clsx(inter.className, 'bg-dot-white/[0.2]','bg-dot-black/[0.2] bg-[var(--backgroundColor)]  text-full transition-all ease-in duration-400 relative')}>
             <div className={`w-full flex gap-20 flex-col `}>
                <Toaster />
                <ClientProvider>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React from "react"
-import Link from "next/link"
 import ThemeSwitcher from "../../../ThemeSwitcher"
 import "./NavBar.css"
 import { useState } from "react"
@@ -13,29 +12,29 @@ function NavBar() {
    const handleClick = () => {
       setIsVisible(!isVisible)
    }
-   
+
    return (
-      <nav className="rounded-b-2xl  navigation-header m-auto p-8 flex justify-between items-center w-full text-xl font-bold mb-3">
+      <nav className="rounded-b-2xl navigation-header m-auto p-8 flex justify-between items-center w-full text-xl font-bold mb-3">
          <div>
             <p>Developer Mohit</p>
          </div>
          <main className="flex gap-5 md:gap-7 relative items-center">
             <div className="hidden md:flex gap-3 md:gap-7">
-               <NavItem dest="/contact" item="Contact" />
+               <NavItem dest="/contact" item="Contact"/>
                <NavItem dest="/" item="About Me" />
             </div>
             <ThemeSwitcher />
             <section className="flex md:hidden">
-               <button onClick={handleClick} className="transition-all ease-in-out duration-300">
+               <button onClick={handleClick} >
                   {!isVisible && <Image
                      src="/Bars.svg"
-                     alt="Bars"
+                     alt="Bars Image"
                      width={15}
                      height={15}
                   />}
                   {isVisible && <Image
                      src="/cross.svg"
-                     alt="Cross"
+                     alt="Cross Image"
                      width={15}
                      height={15}
                   />}
@@ -43,16 +42,8 @@ function NavBar() {
             </section>
             {isVisible && (
                <div className="mt-5 InvisibleNavBar flex w-max p-5 flex-col gap-7 absolute top-full right-0  rounded-lg py-8">
-                  <Link href={'/contact'}>
-                     <p>
-                        Contact
-                     </p>
-                  </Link>
-                  <Link href={'/'}>
-                     <p>
-                        About Me
-                     </p>
-                  </Link>
+                  <NavItem dest="/contact" item="Contact" />
+                  <NavItem dest="/" item="About Me" />
                </div>
             )}
          </main>
